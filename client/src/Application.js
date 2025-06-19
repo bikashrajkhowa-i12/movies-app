a5d09faf72e7cef17126346fea5873fd8574bb4d
bikashrajkhowa-i12 <bikashrajkhowa424265@gmail.com>
2025-06-19 17:23:03 +0530

    feat: :sparkles: Build the basic movie UI poster with raw data.

--- client/src/Application.js
+++ client/src/Application.js
@@ -1,17 +1,51 @@
 import { useState, useEffect } from "react";
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
+import { get, isEmpty } from "lodash";
 
-import callApi from './api/callApi';
+import callApi from "./api/callApi";
 
-import Footer from './components/Footer';
-import Navbar from './components/Navbar';
-import movies from './sample/temp';
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import movies from "./sample/temp";
+import { tmdbImageBaseUrl, imgNotFoundUrl } from "./sample/utils";
 
-function TrendingBanner () {
-  return (<></>)
+function TrendingBanner(data) {
+  const [index, setIndex] = useState(0);
+
+  const { movies = [] } = data;
+  const trendingList = movies.length > 10 ? movies.slice(0, 8) : movies;
+
+  const next = () => {
+    setIndex((index + 1) % trendingList.length);
+  };
+  const prev = () => {
+    setIndex((index - 1 + trendingList.length) % trendingList.length);
+  };
+
+  const poster = trendingList[index];
+  const imgSrc = !isEmpty(`${get(poster, "poster_path")}`)
+    ? `${tmdbImageBaseUrl}/${get(poster, "poster_path")}`
+    : `${imgNotFoundUrl}`;
+
+  return (
+    <div className="poster">
+      <img src={imgSrc} className="poster-image"/>
+      <div className="poster-overlay"></div>
+      <div className="poster-content">
+        <h2 className="poster-title">{poster?.title || 'Unknown title!'}</h2>
+        <p className="poster-overview">{poster?.overview || 'Unknown overview!'}</p>
+        <div className="poster-buttons">
+          <button className="poster-info-button">ℹ More Info</button>
+        </div>
+      </div>
+      <button className="nav-arrow left" onClick={prev}><FaChevronLeft/></button>
+      <button className="nav-arrow right" onClick={next}><FaChevronRight/></button>
+    </div>
+  );
 }
 
-function MovieRow () {
-  return (<></>)
+function MovieRow() {
+  return <></>;
 }
 
 function Application() {
@@ -22,10 +56,10 @@ function Application() {
 
   return (
     <div>
-      <Navbar movies={movies}/>
-      <TrendingBanner movies={movies.length? movies : []}/>
-      <MovieRow title="Popular releases"/>
-      <Footer/>
+      <Navbar movies={movies} />
+      <TrendingBanner movies={movies.length ? movies : []} />
+      <MovieRow title="Popular releases" />
+      <Footer />
     </div>
   );
 }
---
+++ client/src/sample/utils.js
@@ -0,0 +1,2 @@
+export const tmdbImageBaseUrl= "https://image.tmdb.org/t/p/w500"
+export const imgNotFoundUrl = "https://upload.wikimedia.org/wikipedia/commons/a/ac/No_image_available.svg"
