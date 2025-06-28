c5a9ab8ac9293003724defdcd88800036e48714e
bikashrajkhowa-i12 <bikashrajkhowa424265@gmail.com>
2025-06-28 16:26:55 +0530

    feat: :sparkles: added and configured abortcontroller for all apis.

--- client/src/api/callApi.js
+++ client/src/api/callApi.js
@@ -4,20 +4,31 @@ import { baseUrl } from "../constants/urlConstants";
 
 const callApi = async (data) => {
   const { url, method = "", payload = {}, params = {} } = data || {};
+  
+  const controller = new AbortController();
+  const signal = controller.signal;
+
+  const axiosConfig = {
+    url: `${baseUrl}/api${url}`,
+    method: method?.toUpperCase() || "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    data: payload,
+    params,
+    signal,
+  };
+
   try {
-    const response = await axios({
-      url: `${baseUrl}/api${url}`,
-      method: method?.toUpperCase() || "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      data: payload,
-      params: params,
-    });
+    const response = await axios(axiosConfig);
     return response;
   } catch (error) {
-    console.log("function: callApi(), \n error: ", error);
-    throw new Error(error?.message || "Something went wrong");
+    if (signal.aborted) {
+      console.log('Request aborted by this AbortController');
+    } else {
+      console.log("function: callApi(), \n error: ", error);
+      throw new Error(error?.message || "Something went wrong");
+    }
   }
 };
 
